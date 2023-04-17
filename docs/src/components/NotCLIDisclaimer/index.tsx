2c433f312a2dfdff8044323f2c3762ed362bf430
adriantpaez <adriantpaez@gmail.com>
2023-04-17 14:21:12 +0200

    feat: add NotCLIDisclaimer component

---
+++ docs/src/components/NotCLIDisclaimer/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function NotCLIDisclaimer() {
+    return (
+        <blockquote><em><strong>Note:</strong> Commands are for demonstration purposes only as the cli tool doesn't exists yet.</em></blockquote>
+    );
+}
