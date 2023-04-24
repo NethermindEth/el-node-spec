49318463cbcc22c7ce1a67a9b4a222c8a66c3c28
adriantpaez <adriantpaez@gmail.com>
2023-04-24 15:00:51 +0200

    fix: update docusaurus config for custom domain

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://nethermindeth.github.io/',
+  url: 'https://middleware.nethermind.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/middleware-spec/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
