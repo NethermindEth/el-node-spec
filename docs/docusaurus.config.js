f94c3469b182237a5e8d952f8c6afed4b744e260
AntiD2ta <mtpotro41@gmail.com>
2023-05-30 17:15:17 +0100

    fix: Bad edit link in documentation pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,14 +47,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/NethermindEth/el-node-spec/edit/main/docs',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/NethermindEth/el-node-spec/edit/main/docs',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
