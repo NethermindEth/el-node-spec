1c35c46e872c81519bd8e7bc5f0f49c402a3439d
AntiD2ta <mtpotro41@gmail.com>
2023-07-12 17:17:46 +0100

    doc: Update landing page
    Changes:
    - Rename 'Eigen Layer' to 'EigenLayer'
    - Add EigenLayer logo

--- docs/src/pages/index.tsx
+++ docs/src/pages/index.tsx
@@ -4,19 +4,22 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
+import eigenLayerPng from '@site/static/img/eigenlayer.png'
 
 import styles from './index.module.css';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+    //<header className={clsx('hero hero--primary', styles.heroBanner)}>
+    <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
+        <img className={clsx(styles.featurePng)} src={eigenLayerPng} />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
           <Link
-            className="button button--secondary button--lg"
+            className={clsx("button button--lg", styles.button)}
             to="/docs/introduction">
             Documentation
           </Link>
